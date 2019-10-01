20219de8d626cf8b84006f503ceebb4d40983a02
Abdul Omar <aahmed@ucsd.edu>
2019-10-01 16:20:07 -0700

    implemented nearest neighbor and BuildKdtree functions

--- src/kdt/KDT.hpp
+++ src/kdt/KDT.hpp
@@ -56,10 +56,31 @@ class KDT {
     virtual ~KDT() { deleteAll(root); }
 
     /** TODO */
-    void build(vector<Point>& points) {}
+    void build(vector<Point>& points) {
+
+      unsinged int size = points->size();
+     //if there is only one point
+      if(size == 1) {         
+        root = new KDNode(points);
+        isize++;
+        iheight++;
+        return;
+      }
+      //else recursively build tree
+      root = buildSubtree(points, 0, size, 0, 0);
+    }
 
     /** TODO */
-    Point* findNearestNeighbor(Point& queryPoint) { return nullptr; }
+    Point* findNearestNeighbor(Point& queryPoint) { 
+      
+      findNNHelper(root, queryPoint,0)
+      
+      
+      
+      
+       
+       
+   }
 
     /** Extra credit */
     vector<Point> rangeSearch(vector<pair<double, double>>& queryRegion) {
@@ -67,20 +88,89 @@ class KDT {
     }
 
     /** TODO */
-    unsigned int size() const { return 0; }
+    unsigned int size() const { return isize; }
 
     /** TODO */
-    int height() const { return 0; }
+    int height() const { return iheight; }
 
   private:
     /** TODO */
     KDNode* buildSubtree(vector<Point>& points, unsigned int start,
                          unsigned int end, unsigned int curDim, int height) {
-        return nullptr;
+      
+      unsigned int middle = (start + end)/2;
+      //base case
+      if(start >= end){
+        return;
+      }
+      //get current dimension for sorting
+      curDim = height % points[0].numDim;
+
+      //fil dimension of current evaluation
+      compareValueAt.dimension = curDim;
+     
+      //sort elements at current dimension
+      points.sort(compareValueAt());
+      //this node holds the median point
+      KDNode* node = new KDNode(points[middle]);
+      //everything else goes to its left and right
+      node->left = buildSubtree(points, start, middle, curDim, height + 1);
+      node->right = buildSubtree(points, middle +1, end, curDim, height + 1);
+      isize++;//increment size
+      return node;//pointer to root;
     }
 
     /** TODO */
-    void findNNHelper(KDNode* node, Point& queryPoint, unsigned int curDim) {}
+    void findNNHelper(KDNode* node, Point& queryPoint, unsigned int curDim) {
+      /*base case,at leaf node*/
+      if(node->left == nullptr && node->right == nullptr){
+        
+        /*current best distance */
+         node->point.setDistToQuery( queryPoint);
+         double thresh = node->point.result;
+         
+         //if we have found a smaller distance
+         if(thresh < threshold){
+
+            //current best guess
+            nearestNeighbor = node->point;
+            threshold = thresh;//update current best distance
+         }
+      }
+      else {
+           //if value in current axis is less, points must lie in the left
+           if(queryPoint->valueAt(curDim) <= node->point.valueAt(curDim)){
+             
+             if(queryPoint->valueAt(curDim) - threshold <= node->point.valueAt(curDim))
+              
+              findNNHelper(node->left, queryPoint, curDim);
+            }
+            if(queryPoitn->valueAt(curDim) + threshold > node->point.valueAt(curDim)){
+
+              findNNHelper(node->right, queryPoint, curDim);
+            }
+         }
+         else {
+
+            if(queryPoitn->valueAt(curDim) + threshold > node->point.valueAt(curDim))
+              
+              findNNHelper(node->right, queryPoint, curDim);
+            }
+            if(queryPoitn->valueAt(curDim) - threshold <= node->point.valueAt(curDim)){
+
+              findNNHelper(node->left, queryPoint, curDim);
+            }
+
+
+         }
+     
+
+
+
+      }
+
+
+    }
 
     /** Extra credit */
     void rangeSearchHelper(KDNode* node, vector<pair<double, double>>& curBB,
@@ -88,7 +178,22 @@ class KDT {
                            unsigned int curDim) {}
 
     /** TODO */
-    static void deleteAll(KDNode* n) {}
+    static void deleteAll(KDNode* n) {
+
+       /* Pseudocode:
+           if current node is null: return;
+           recursively delete left sub-tree
+           recursively delete right sub-tree
+           delete current node
+        */
+        if (!n) {
+            return;
+        }
+
+        deleteAll(n->left);   // delete left subtree
+        deleteAll(n->right);  // delete right subtree
+        delete n;             //delete root 
+    }
 
     // Add your own helper methods here
 };
